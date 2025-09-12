d16df2172d57c03604df6e43d0406f81b313be60
Anzar Syahid <zarchp@gmail.com>
2025-09-12 17:26:38 +0700

    feat: generate sitemap

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,13 @@
 
 declare(strict_types=1);
 
+use App\Console\Commands\GenerateSitemap;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function (): void {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command(GenerateSitemap::class)->weekly();
